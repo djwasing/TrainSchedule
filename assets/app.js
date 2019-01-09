8f4f2c0706eb6436f7510d0d1aae019ad9a0b66e
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2019-01-09 13:54:15 -0600

    getting info from firebase and writing to page

--- assets/app.js
+++ assets/app.js
@@ -13,7 +13,7 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$(document).on("click", "#submit", function() {
+$(document).on("click", "#submit", function() {         //on click, getting values of input fields, 
     
     var newName = $("#newName").val().trim();
     var newDest = $("#newDest").val().trim();
@@ -21,7 +21,7 @@ $(document).on("click", "#submit", function() {
     var newFreq = $("#newFreq").val().trim();
 
     function newTrainData(trainName, trainDestination, trainTime, trainFrequency) {
-        database.ref().push({
+        database.ref().push({                       //pushing the newTrainData to the firebase
             name: trainName,
             destination: trainDestination,
             time: trainTime,
@@ -30,13 +30,38 @@ $(document).on("click", "#submit", function() {
         });
     };
 
-    newTrainData(newName, newDest, newTime, newFreq);
+    newTrainData(newName, newDest, newTime, newFreq);       //calling func
 
-    $("#newName").val("");
+    $("#newName").val("");          //resetting the input fields to blank
     $("#newDest").val("");
     $("#newTime").val("");
     $("#newFreq").val("");
 
     console.log("New Data Added");
 
+});
+
+    // contact firebase and return info in order from oldest to newest
+database.ref().orderByChild("dateAdded").limitToLast(10).on("child_added", function (snapshot) {
+
+    console.log(snapshot);
+    var newRow = $("<tr>");         //creates new row within the table
+    var listName = $("<th>");       //creates new header cell within row
+    var listDest = $("<td>");       //creates new cell within the row
+    var listFreq = $("<td>");       //creates new cell within the row
+
+    //giving new row info from firebase and attrs, 
+    //then adding the new row to the table
+    
+    listName.text(snapshot.val().name).attr("scope", "row").attr("id", "name");
+
+    listDest.text(snapshot.val().destination).attr("id", "destination");
+
+    listFreq.text(snapshot.val().frequency).attr("id", "frequency");
+    
+    newRow.append(listName, listDest, listFreq);
+
+    $("#table").append(newRow);
+    
+    
 });
